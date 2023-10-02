d52255c046337dcccd1b5138da20a98eeb9e6b72
Andres Ruales <danielandresruales@hotmail.com>
2023-10-01 19:48:15 -0500

    Se realiza un Hola Mundo con Spring, con Thymeleaf

--- src/main/java/com/HolaMundo/ControladorInicio.java
+++ src/main/java/com/HolaMundo/ControladorInicio.java
@@ -2,6 +2,7 @@ package com.HolaMundo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ControladorInicio {
     
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model model) {
         //Informacion del salo del servidor, con log
+        
+        var mensaje = "Hola mundo con Thymeleaf";
         log.info("Ejecuntando el controlador  Spring MVC");
+        //Llave, valor
+        model.addAttribute("mensaje", mensaje);
+        
+        
         return "index";
     }
     
