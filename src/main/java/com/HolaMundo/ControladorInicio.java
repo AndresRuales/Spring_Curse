ab3e20c76d14912acf461e67af340dc2fa25d4e9
Andres Ruales <danielandresruales@hotmail.com>
2023-10-01 21:03:45 -0500

    Se usa Value para digitar en HTML

--- src/main/java/com/HolaMundo/ControladorInicio.java
+++ src/main/java/com/HolaMundo/ControladorInicio.java
@@ -1,6 +1,7 @@
 package com.HolaMundo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
+    
+    @Value("${index.saludo}")
+    private String saludo;
+    
     
     @GetMapping("/")
     public String inicio(Model model) {
@@ -17,6 +22,7 @@ public class ControladorInicio {
         log.info("Ejecuntando el controlador  Spring MVC");
         //Llave, valor
         model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         
         
         return "index";
